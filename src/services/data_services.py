bcbcead340470e33e31c4ee3592844cddc5cd8c7
Abner <abner.a.abreu.tamayo@gmail.com>
2026-07-15 05:11:35 -0400

    feat(services): add data_services
    add basic CRUD services to data_services
    add get_models method to data_services

---
+++ src/services/data_services.py
@@ -0,0 +1,68 @@
+from peewee import DoesNotExist, IntegrityError
+from database import Model,Data,ModelData,db
+from typing import List,Optional,Dict,Any
+import logging
+
+from .base_services import BaseServices
+
+logger = logging.getLogger(__name__)
+
+class DataService(BaseServices):
+    def create(self,name:str,place:str,date):
+        try:
+            data = Data.create(name=name,place=place,date=date)
+            logger.info(f"Param created: {name}")
+
+            return data
+        
+        except IntegrityError as e:
+            logger.error(f"Error when creating data {name}: {e}")
+            raise ValueError(f"A Data with name '{name}' already exists")
+        
+    def get_by_id(self,name:str) -> Data:
+        try:
+            return Data.get_by_id(name)
+        except DoesNotExist:
+            logger.warning(f"Data {name} not finded")
+            raise ValueError(f"Data {name} not finded")
+        
+    def to_list(self,filters: Optional[Dict[str,Any]] = None) -> List[Data]:
+
+        query = Data.select()
+
+        if filters:
+            for field,value in filter:
+                if field == 'name__contains':
+                    query = query.where(Data.name.contains(value))
+                elif field == 'name__startswith':
+                    query = query.where(Data.name.startswith(value))
+
+        query = query.order_by(Data.name.asc())
+        
+        return list(query)
+    
+    def update(self):
+        return
+    
+    def delete(self, name:str):
+        data = self.get_by_id(name)
+
+        with db.atomic():
+            ModelData.delete().where(
+                ModelData.data == data
+            ).execute()
+            
+            data.delete_instance()
+        
+        logger.info(f"Data deleted: {name}")
+        return True
+    
+    def get_models(self,name:str) -> List[Model]:
+        data = self.get_by_id(name)
+
+        query = (ModelData
+                 .select(ModelData,Model)
+                 .join(Model)
+                 .where(ModelData.data == data))
+        
+        return list(query)
