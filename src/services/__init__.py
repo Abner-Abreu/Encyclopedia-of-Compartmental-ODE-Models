c5edd6a1706f03123fac4e8e89902902c3c9429a
Abner <abner.a.abreu.tamayo@gmail.com>
2026-07-15 23:13:58 -0400

    feat(services): add service_handler
    add ServiceHandler to module init

--- src/services/__init__.py
+++ src/services/__init__.py
@@ -5,7 +5,10 @@ from .data_services import DataService
 from .param_services import ParamService
 from .situation_services import SituationService
 from .validation_services import ValidationService
+from .compartment_services import CompartmentService
+from .service_handler import ServiceHandler
 
 __all__ = ["ModelService", "ArticleService", 
            "DataService", "ParamService", 
-           "SituationService", "ValidationService"]
+           "SituationService", "ValidationService",
+           "CompartmentService", "ServiceHandler"]
---
+++ src/services/service_handler.py
@@ -0,0 +1,82 @@
+from .article_services import ArticleService
+from .data_services import DataService
+from .model_services import ModelService
+from .param_services import ParamService
+from .situation_services import SituationService
+from .validation_services import ValidationService
+from .compartment_services import CompartmentService
+
+from typing import Dict, Any
+from datetime import date
+
+class ServiceHandler:
+    def __init__(self):
+        self.article = ArticleService
+        self.data = DataService
+        self.model = ModelService
+        self.param = ParamService
+        self.situation = SituationService
+        self.validation = ValidationService
+        self.compartment = CompartmentService
+
+    def create_complete(self,
+                        name:str,
+                        compartments: list[Dict[str,str]],
+                        parameters: list[Dict[str,Any]],
+                        situation_name: str,
+                        situation_description: str,
+                        article_name: str,
+                        article_author: str,
+                        article_date: str,
+                        data_name: str,
+                        data_place: str,
+                        data_date: str):
+        
+        #Basic validation
+        if (self.validation.IsValidName(name) == False):
+            return False
+        elif self.validation.IsValidName(situation_name) == False:
+            return False
+        elif self.validation.IsValidName(article_name) == False:
+            return False
+        elif self.validation.IsValidName(article_author) == False:
+            return False
+        elif self.validation.IsValidName(data_name) == False:
+            return False
+        elif self.validation.IsValidDate(article_date) == False:
+            return False
+        elif self.validation.IsValidDate(data_date) == False:
+            return False
+        try:
+            self.model.get_by_id(name)
+            return False
+        except:
+            self.validate_compartments(compartments)
+            self.validate_parameters(parameters)
+
+        
+        
+        return
+
+
+        
+        
+
+    def validate_compartments(self,compartments:list[Dict[str,Any]]):
+        for comp in compartments:
+            try:
+                self.compartment.get_by_id(comp['name'])
+                return False
+            except:
+                continue
+        return True
+    
+    def validate_parameters(self,parameters:list[Dict[str,Any]]):
+        for param in parameters:
+            try:
+                self.compartment.get_by_id(param['name'])
+                return False
+            except:
+                continue
+        return True
+        
